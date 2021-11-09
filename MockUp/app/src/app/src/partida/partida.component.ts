dda2185ca8409f7210b6c67788661a2269346d9a
Álvaro Vargas Molina <36602950+AlvaroVargasM@users.noreply.github.com>
2021-11-08 23:42:08 -0600

    Added the get rules on partida component

--- MockUp/app/src/app/src/partida/partida.component.ts
+++ MockUp/app/src/app/src/partida/partida.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ConnectionService } from 'src/app/connection.service';
+import { Rules } from '../models/rules.model';
 
 @Component({
   selector: 'app-partida',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./partida.component.css']
 })
 export class PartidaComponent implements OnInit {
+  eventRules = new Rules('',0,0,'',0,0,'')
+  rootEventRulesGetURL:string = 'http://localhost:5000/api/Partidas/GetReglasDelEvento/'
 
-  constructor() { }
+  constructor(private service: ConnectionService) { }
 
   ngOnInit(): void {
   }
 
+  confirmedToken(eventCode: any): void {
+    let url = this.rootEventRulesGetURL + eventCode.toString();
+    console.log(url);
+    this.service.Get(url).subscribe(
+      response => {
+          console.log(response);
+          this.eventRules =  response;
+          console.log(this.eventRules);
+      }
+    );
+  }
 }
