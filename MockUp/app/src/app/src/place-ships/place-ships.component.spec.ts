ce8dc58ac2f0a42af29c4107616ef627984dbe21
Álvaro Vargas Molina <36602950+AlvaroVargasM@users.noreply.github.com>
2021-11-18 19:31:34 -0600

    Added some unit tests for place-ships

--- MockUp/app/src/app/src/place-ships/place-ships.component.spec.ts
+++ MockUp/app/src/app/src/place-ships/place-ships.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Ship } from '../models/ship.model';
 import { PlaceShipsComponent } from './place-ships.component';
 
 describe('PlaceShipsComponent', () => {
@@ -21,5 +21,35 @@ describe('PlaceShipsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  // Tests
+  it('should rotate', function () {
+    let h = component.isHorizontal;
+    component.rotate();
+    expect(h).not.toBe(component.isHorizontal);
+  });
+
+  it('should not rotate', function () {
+    let h = component.isHorizontal;
+    component.isShipSelected = true;
+    component.rotate();
+    expect(h).toBe(component.isHorizontal);
+  });
+
+  it('should allow a correct position', function () {
+    component.width = 10;
+    component.height = 10;
+
+    component.selectedShip = new Ship('destroyer', 3, 'orange');
+    expect(component.isPositionCorrect(1)).toBeTrue();
+  });
+
+  it('should not allow a position because the ship is out of bounds', function () {
+    component.width = 10;
+    component.height = 10;
+
+    component.selectedShip = new Ship('destroyer', 3, 'orange');
+    expect(component.isPositionCorrect(100)).toBeFalse();
   });
 });
